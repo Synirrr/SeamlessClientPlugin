5d489f88b869a792d2918bb633df4f5593cc6975
Bob Da Ross <52760019+BobDaRoss@users.noreply.github.com>
2021-06-17 00:42:43 -0500

    Updated modloader comment

--- SeamlessTransfer/ModLoader.cs
+++ SeamlessTransfer/ModLoader.cs
@@ -10,7 +10,14 @@ namespace SeamlessClientPlugin.SeamlessTransfer
     {
         /*  Mod loader should download and load missing mods for target server, and unload ones that arent needed
          * 
+         * Sandbox.Game.World.MyScriptManager.LoadData() is where modded scripts get loaded and added
+         * Sandbox.Game.World.MySession() calls MyDefinitionManager.Static.LoadData(mods); which loads mod data files
          * 
+         * 
+         *  Need to be called in the following order: 
+         *  ScriptManager.Init(checkpoint.ScriptManagerData);
+	     *  MyDefinitionManager.Static.LoadData(checkpoint.Mods);
+	     *  PreloadModels(sector);
          * 
          * 
          */
