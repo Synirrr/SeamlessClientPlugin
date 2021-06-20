a4dc98231b0d118540b0d112abc63ce8f2d4cf47
Bob Da Ross <52760019+BobDaRoss@users.noreply.github.com>
2021-06-19 21:58:10 -0500

    Updated assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SeamlessClientPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A seamless client plugin for Nexus compatible servers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Casimir")]
 [assembly: AssemblyProduct("SeamlessClientPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.1")]
+[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
